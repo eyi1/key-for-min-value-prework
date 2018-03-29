7f4be13f94467c898484380e65e7a41d03b18c8f
eyi1 <ej7729@gmail.com>
2018-03-29 02:04:50 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
 
   lowest_key =name_hash.first[0]
   lowest_val =name_hash.first[1]
-    name_hash.collect |name, value|
+    name_hash.each do |name, value|
       if value < lowest_val
         lowest_val = value
         lowest_key = name
